a634d5d4451a901ed38986e2e46ddb89013cee4f
Daniel <daanilo2497@gmail.com>
2021-11-30 14:12:44 -0300

    Cambio configuracion

--- src/main/java/com/aninfo/SupportApp.java
+++ src/main/java/com/aninfo/SupportApp.java
@@ -37,7 +37,7 @@ public class SupportApp {
 				.paths(PathSelectors.any())
 				.build();
 	}
-// blabla
+
 	public static void main(String[] args) {
 		SpringApplication.run(SupportApp.class, args);
 	}
