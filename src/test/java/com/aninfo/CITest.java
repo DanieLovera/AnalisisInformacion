6f20aa057eb021b7b1e6d463218a74b16f690d34
juanimedone <jmedone@fi.uba.ar>
2021-12-03 22:49:26 -0300

    Pruebo CI con test que rompe

---
+++ src/test/java/com/aninfo/CITest.java
@@ -0,0 +1,12 @@
+package com.aninfo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CITest {
+    @Test
+    void dummieTest() {
+        assertTrue(false);
+    }
+}
